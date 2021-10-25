6ae56d252049a1433ac9acbf38cfdf3b485625ad
Gaetan-TREUSCHEL <57520481+Gaetan-TREUSCHEL@users.noreply.github.com>
2021-10-25 16:48:48 +0200

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-$("#form").submit(function(){
-  if ($("#nom").val() === "correct"){
-    $("#spanInfo").text( "Validated...").show();
-    return;
-  }
-
-  $("#spanInfo").text("Not valid!").show().fadeOut(1000);
-});
